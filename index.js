bbbd7502f60ae3e16ab0e2f9de6783242c734778
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-12 19:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const items = (collection instanceof Array)? collection : Object.values(collection);
 
+      for(const item of items){
+        callback(item);
+      }
+      return collection;
     },
 
     map: function() {
